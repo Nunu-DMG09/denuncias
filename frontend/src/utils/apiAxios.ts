53e145a1995d5cfb719c68f860fb2e4a0b02a067
Diego17 <diegoalbertocastropastor@gmail.com>
2025-03-12 08:56:16 -0500

    add response interceptors to apiAxios for improved error handling

--- frontend/src/utils/apiAxios.ts
+++ frontend/src/utils/apiAxios.ts
@@ -1,10 +1,25 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'backend_url', // placeholder de la url del backend
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    timeout: 5000,
-})
+	baseURL: "backend_url", // placeholder de la url del backend
+	headers: {
+		"Content-Type": "application/json",
+	},
+	timeout: 5000,
+});
+api.interceptors.response.use(
+	(response) => response,
+	(error) => {
+		if (error.code === "ECONNABORTED") {
+			console.error("Timeout error");
+		} else if (error.response) {
+			console.error(
+				`Error response: ${error.response.status} : ${error.response.data}`
+			);
+		} else {
+			console.error(`Error message: ${error.message}`);
+		}
+		return Promise.reject(error);
+	}
+);
 export default api;
