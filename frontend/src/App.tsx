bacbd096f74430e490edaf81fd9a6444b3aaa76f
Diego17 <diegoalbertocastropastor@gmail.com>
2025-03-15 12:59:21 -0500

    enhance App component styling and update index.css background

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,15 @@ import './App.css'
 
 function App() {
   return (
-    <>
-      hola mundo
-    </>
+    <main style={{
+      backdropFilter: 'blur(11px) saturate(70%)',
+      WebkitBackdropFilter: 'blur(11px) saturate(50%)',
+      backgroundColor: '#002F590D',
+      borderRadius: '12px',
+      padding: '5rem',
+    }}>
+      <h1 className='text-slate-100'>Wazaaaaaaaaaa</h1>
+    </main>
   )
 }
 
