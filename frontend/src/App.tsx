d62da495956e14cc0cfe8b78550cf1c25d055523
Diego17 <diegoalbertocastropastor@gmail.com>
2025-03-15 18:18:09 -0500

    refactor: extract header into separate component for better structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,12 @@
 import { useState } from "react";
 import "./App.css";
+import Header from "./Components/Header";
 
 function App() {
 	const [selectedReason, setSelectedReason] = useState<number | null>(0);
 	return (
 		<div className="min-h-screen">
-			<header className="header shadow-md w-full bg-white py-4 px-4">
-				<nav className="nav-container">
-					<div className="nav-content flex items-center gap-4">
-						<a
-							className="logo-section flex items-center gap-4"
-							href="https://www.munijlo.gob.pe/web/"
-							target="_blank"
-							rel="noreferrer"
-						>
-							<img
-								alt="Logo Municipal"
-								className="nav-logo w-12 h-16 object-contain"
-								src="/logo.jpeg"
-							/>
-							<h3 className="font-semibold flex flex-col text-lg text-gray-700">
-								<span>Municipalidad Distrital de</span>
-								<span>José Leonardo Ortiz</span>
-							</h3>
-						</a>
-					</div>
-				</nav>
-			</header>
-
+			<Header />
 			<main className="container mx-auto px-4 pt-28 pb-8">
 				<h1 className="text-4xl font-bold text-center mb-8 text-gray-800">
 					Realizar Denuncias sobre Fraude
--- frontend/src/Components/Header.tsx
+++ frontend/src/Components/Header.tsx
@@ -1,23 +1,30 @@
-import React from "react";
 import "../header.css"; 
-
 const logo = "/logo.jpeg"; 
 
-const Header: React.FC = () => {
+const Header = () => {
   return (
-    <header className="header">
-      <div className="header__content">
-        {/* Envolver el logo y el texto en un <a> */}
-        <a href="https://www.munijlo.gob.pe/web/" target="_blank" rel="noopener noreferrer" className="header__logo">
-          <img src={logo} alt="Logo Municipalidad" />
-          <h3 className="header__text">
-            <span>Municipalidad Distrital de</span>
-            <span>José Leonardo Ortiz</span>
-          </h3>
-        </a>
-        <button className="header__button">Ver estado de denuncia</button>
-      </div>
-    </header>
+    <header className="header shadow-md w-full bg-white py-4 px-4">
+				<nav className="nav-container">
+					<div className="nav-content flex items-center gap-4">
+						<a
+							className="logo-section flex items-center gap-4"
+							href="https://www.munijlo.gob.pe/web/"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<img
+								alt="Logo Municipal"
+								className="nav-logo w-12 h-16 object-contain"
+								src={logo}
+							/>
+							<h3 className="font-semibold flex flex-col text-lg text-gray-700">
+								<span>Municipalidad Distrital de</span>
+								<span>José Leonardo Ortiz</span>
+							</h3>
+						</a>
+					</div>
+				</nav>
+			</header>
   );
 };
 
