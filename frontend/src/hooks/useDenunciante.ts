3d987a8836f0cf1b4e383113986178ab67836586
Diego17 <diegoalbertocastropastor@gmail.com>
2025-03-17 12:11:50 -0500

    feat: enhance DatosDenunciado and DatosDenunciante components with document type and gender selection

--- frontend/src/hooks/useDenunciante.ts
+++ frontend/src/hooks/useDenunciante.ts
@@ -2,8 +2,19 @@ import { useState } from "react";
 
 export const useDenunciante = () => {
 	const [tipoDatos, setTipoDatos] = useState<string>("datos-personales");
+	const [tipoDocumento, setTipoDocumento] = useState("");
+	const [sexo, setSexo] = useState<string>("");
+
 	const handleTipoDatos = (tipo: string) => {
 		setTipoDatos(tipo);
 	};
-	return { tipoDatos, handleTipoDatos };
+	const handleTipoDocumento = (tipo: string) => {
+		setTipoDocumento(tipo);
+	}
+
+	const handleSexo = (sexo: string) => {
+		setSexo(sexo);
+	}
+
+	return { tipoDatos, handleTipoDatos, tipoDocumento, handleTipoDocumento, sexo, handleSexo };
 };
