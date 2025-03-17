45e70b1e1065d89f7b8dfac206491621a1c3c9b4
Julian Burga Bracamonte <julianburgabracamonte@gmail.com>
2025-03-17 11:17:45 -0500

    api

--- backend/app/Config/Routes.php
+++ backend/app/Config/Routes.php
@@ -6,3 +6,8 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+// Rutas para la API
+$routes->group('api', function ($routes) {
+    $routes->get('dni/(:num)', 'ConsultaApi::buscarDNI/$1');
+    $routes->get('ruc/(:num)', 'ConsultaApi::buscarRUC/$1');
+});
