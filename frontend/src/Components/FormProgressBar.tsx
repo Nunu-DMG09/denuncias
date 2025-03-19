6e5c32d50036c66250f50272ca651a1e51bdafb8
Diego17 <diegoalbertocastropastor@gmail.com>
2025-03-19 10:36:46 -0500

    feat: add FormProgressBar component to enhance form navigation experience

---
+++ frontend/src/Components/FormProgressBar.tsx
@@ -0,0 +1,31 @@
+import { useFormContext } from "../hooks/useFormContext";
+
+export const FormProgressBar = () => {
+    const { currentPage } = useFormContext();
+	return (
+		<div className="mb-8">
+			<div className="flex justify-between mb-2">
+				{["Detalles", "Denunciado", "Denunciante", "Resumen"].map(
+					(step, index) => (
+						<div
+							key={index}
+							className={`text-sm text-center ${
+								currentPage === index + 1
+									? "font-bold text-(--primary-color)"
+									: "text-gray-500"
+							}`}
+						>
+							{step}
+						</div>
+					)
+				)}
+			</div>
+			<div className="w-full bg-gray-200 h-2 rounded-full">
+				<div
+					className="bg-(--primary-color) h-2 rounded-full transition-all duration-300"
+					style={{ width: `${(currentPage / 4) * 100}%` }}
+				></div>
+			</div>
+		</div>
+	);
+};
