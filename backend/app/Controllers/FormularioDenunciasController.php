fb344e84349dc1e598954b618eb6b57540dfd9d0
Julian Burga Bracamonte 🤓 <julianburgabracamonte@gmail.com>
2025-03-18 22:20:42 -0500

    Update FormularioDenunciasController.php

--- backend/app/Controllers/FormularioDenunciasController.php
+++ backend/app/Controllers/FormularioDenunciasController.php
@@ -39,6 +39,7 @@ class FormularioDenunciasController extends ResourceController
     function create()
     {
         $formData = $this->request->getJSON(true);
+        // echo json_encode($formData);
         log_message('debug', 'Datos recibidos: ' . json_encode($formData));
         return $this->response->setJSON([
             'success' => true,
