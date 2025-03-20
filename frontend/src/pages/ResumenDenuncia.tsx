4c3265d7206d17d55c5671aa2052374f1bf0a663
Diego17 <diegoalbertocastropastor@gmail.com>
2025-03-20 08:08:08 -0500

    Refactor ResumenDenuncia component to improve styling and utilize CSS variables for colors

--- frontend/src/pages/ResumenDenuncia.tsx
+++ frontend/src/pages/ResumenDenuncia.tsx
@@ -5,8 +5,6 @@ import * as Icons from '../Components/Icons';
 export const ResumenDenuncia = () => {
 	const { formData } = useFormContext();
 	const [copied, setCopied] = useState(false);
-
-	// Supongamos que el código de seguimiento viene del contexto
 	const trackingCode = formData.tracking_code || "#123456";
 	console.log(formData);
 	console.log(trackingCode);
@@ -19,7 +17,6 @@ export const ResumenDenuncia = () => {
 
 	return (
 		<div id="confirmation-page" className="space-y-8 max-w-3xl mx-auto">
-			{/* Encabezado con icono de éxito */}
 			<div className="space-y-4 text-center">
 				<div className="mx-auto w-20 h-20 bg-green-100 rounded-full flex items-center justify-center">
 					<Icons.CompletedIcon />
@@ -32,23 +29,21 @@ export const ResumenDenuncia = () => {
 					brevedad.
 				</p>
 			</div>
-
-			{/* Sección de código de seguimiento */}
-			<div className="space-y-4 bg-white p-6 rounded-lg shadow-md border border-gray-100">
+			<div className="space-y-4 p-6">
 				<p className="text-gray-700 font-medium text-center">
 					Código de seguimiento de denuncia
 				</p>
 				<div className="flex flex-col md:flex-row overflow-hidden rounded-lg shadow-sm border border-gray-200">
 					<div className="bg-blue-50 flex-grow p-4 flex items-center justify-center">
-						<p className="text-2xl font-mono font-semibold text-blue-700">
+						<p className="text-2xl font-mono font-semibold text-(--primary-color)">
 							{trackingCode}
 						</p>
 					</div>
 					<button
 						onClick={copyToClipboard}
-						className={`p-4 ${
-							copied ? "bg-green-600" : "bg-blue-600"
-						} text-white transition-colors duration-300 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 flex items-center justify-center`}
+						className={`p-4 cursor-pointer ${
+							copied ? "bg-green-600 hover:bg-green-400" : "bg-(--secondary-color) hover:bg-(--primary-color)"
+						} text-white transition-colors duration-300 focus:outline-none flex items-center justify-center`}
 					>
 						{copied ? (
 							<>
@@ -70,7 +65,9 @@ export const ResumenDenuncia = () => {
 			</div>
 			<div className="bg-blue-50 border-l-4 border-(--primary-color) p-5 rounded-r-lg shadow-sm">
 				<div className="flex">
-					<div className="flex-shrink-0"></div>
+					<div className="flex-shrink-0">
+						<Icons.InfoIcon />
+					</div>
 					<div className="ml-3">
 						<p className="text-sm text-gray-700">
 							Revisaremos tu denuncia al detalle y, de ser el
