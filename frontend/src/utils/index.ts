841016ddd202de059886b3c0213050373c5f962b
Diego17 <diegoalbertocastropastor@gmail.com>
2025-03-21 15:51:22 -0500

    Fix date format in formatDate utility to use DD/MM/YYYY format

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -197,5 +197,5 @@ export const formatDate = (date: Date) => {
 	const month = (date.getMonth() + 1).toString().padStart(2, "0");
 	const year = date.getFullYear();
 
-	return `${year}/${month}/${day}`;
+	return `${day}/${month}/${year}`;
 }
