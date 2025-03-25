dfd303d806f5253c66b3335581d00743316ac476
Diego17 <diegoalbertocastropastor@gmail.com>
2025-03-25 17:04:14 -0500

    Add logout button to header for admin section

--- frontend/src/Components/Header.tsx
+++ frontend/src/Components/Header.tsx
@@ -1,10 +1,12 @@
 const logo = "/logo.jpeg";
 import { Link, useLocation } from "react-router";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 const Header = () => {
-
+	const { logout } = useAuthContext();
 	const location = useLocation();
 	const isFormPage = location.pathname === "/";
+	const isAdminSection = location.pathname.includes("/admin");
 
 	return (
 		<header className="header shadow-md w-full bg-white py-4 px-4">
@@ -24,7 +26,14 @@ const Header = () => {
 							<span>José Leonardo Ortiz</span>
 						</h3>
 					</Link>
-					{isFormPage ? (
+					{isAdminSection ? (
+						<button
+							onClick={logout}
+							className="bg-red-400 text-sm md:text-base text-center text-white font-semibold px-4 py-3 rounded-lg ml-17 cursor-pointer hover:scale-110 hover:bg-red-800 transition-all ease-in-out duration-300"
+						>
+							Cerrar Sesión
+						</button>
+					) : isFormPage ? (
 						<Link
 							to="/tracking-denuncia"
 							className="bg-(--secondary-color) text-sm md:text-base text-center text-white font-semibold px-4 py-3 rounded-lg ml-17 cursor-pointer hover:scale-110 hover:bg-(--primary-color) transition-all ease-in-out duration-300"
