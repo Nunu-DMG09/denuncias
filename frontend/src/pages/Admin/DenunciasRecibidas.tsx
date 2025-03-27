19866b43df62abb4525191cea80c97e453a35a7a
Diego17 <diegoalbertocastropastor@gmail.com>
2025-03-27 09:37:46 -0500

    Implement pagination for received complaints in DenunciasRecibidas component

--- frontend/src/pages/Admin/DenunciasRecibidas.tsx
+++ frontend/src/pages/Admin/DenunciasRecibidas.tsx
@@ -1,29 +1,32 @@
-import { Link } from "react-router";
+import { useAdminDenunciasRecibidas } from "../../hooks/Admin/Denuncias/useAdminDenunciasRecibidas";
 
 export const DenunciasRecibidas = () => {
+
+    const itemsPerPage = 10;
+    const {
+        loading,
+        denunciasPaginadas,
+        totalPages,
+        currentPage,
+        handleCurrentPage,
+        getVisiblePageNumbers,
+        handlePageChange,
+    } = useAdminDenunciasRecibidas(itemsPerPage);
+
     return (
-        <div className="flex flex-col items-center justify-center min-h-[70vh] px-4">
-            <h1 className="text-6xl font-bold text-gray-800 mb-4">Denuncias Recibidas</h1>
-            <h2 className="text-3xl font-semibold text-gray-700 mb-6">
-                En construcción
-            </h2>
-            <p className="text-lg text-gray-600 max-w-md text-center mb-8">
-                Estamos trabajando en esta sección, por favor regrese más tarde.
-            </p>
-            <div className="flex gap-4">
-                <Link
-                    to="/"
-                    className="bg-(--secondary-color) text-white px-6 py-3 rounded-lg hover:bg-(--primary-color) transition-all duration-300 ease-in-out"
-                >
-                    Volver al inicio
-                </Link>
-                <Link
-                    to="/admin/dashboard"
-                    className="bg-gray-200 text-gray-800 px-6 py-3 rounded-lg hover:bg-gray-300 transition-all duration-300 ease-in-out"
-                >
-                    Ir al Dashboard
-                </Link>
-            </div>
+        <div className="container mx-auto my-8 px-4">
+			<div className="flex justify-between items-center mb-6">
+				<h2 className="text-2xl font-bold text-gray-800">
+					Denuncias Recibidas
+				</h2>
+				{denunciasPaginadas.length > 0 && (
+					<div className="text-sm text-gray-600">
+						Mostrando {denunciasPaginadas.length} de{" "}
+						{denunciasPaginadas.length} denuncias
+					</div>
+				)}
+			</div>
+            
         </div>
     );
 }
