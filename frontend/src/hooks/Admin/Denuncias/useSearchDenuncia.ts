5aff2367b4dc933ddcd746dd811e713b436ec547
Diego17 <diegoalbertocastropastor@gmail.com>
2025-04-01 09:45:44 -0500

    Implement search functionality for 'Denuncia' with document type selection and data fetching

---
+++ frontend/src/hooks/Admin/Denuncias/useSearchDenuncia.ts
@@ -0,0 +1,193 @@
+import { useCallback, useEffect, useState } from "react";
+import { getDNIData, getRUCData } from "../../../services/apisDocs";
+import { toast } from "sonner";
+import { authApi } from "../../../utils/apiAxios";
+import { useFormContext } from "../../Form/useFormContext";
+
+interface Denuncias {
+	id: string;
+	tracking_code: string;
+	motivo_id: string;
+	motivo: string;
+	descripcion: string;
+	fecha_registro: string;
+	estado: string;
+}
+
+export const useSearchDenuncia = () => {
+	const { motivos } = useFormContext();
+	const [tipoDocumento, setTipoDocumento] = useState<string>("");
+	const [numeroDocumento, setNumeroDocumento] = useState<string>("");
+	const [nombre, setNombre] = useState<string>("");
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+	const [denunciaData, setDenunciaData] = useState<Denuncias[]>([]);
+	const [error, setError] = useState<string | null>(null);
+
+	const handleTipoDocumento = (tipo: string) => {
+		setTipoDocumento(tipo);
+		setNumeroDocumento("");
+		setNombre("");
+		setError(null);
+	};
+	const handleDocumentoChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const documentoValue = e.target.value;
+		setNumeroDocumento(documentoValue);
+	};
+
+	const handleName = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const nameValue = e.target.value;
+		setNombre(nameValue);
+	};
+	useEffect(() => {
+		if (tipoDocumento === "dni" && numeroDocumento.length !== 8) {
+			setNombre("");
+		}
+		if (tipoDocumento === "dni" && numeroDocumento.length === 8 && nombre) {
+			return;
+		}
+		if (tipoDocumento === "dni" && numeroDocumento.length === 8) {
+			const fetchDniData = async () => {
+				setIsLoading(true);
+				setError(null);
+				try {
+					const nombre = await getDNIData(numeroDocumento);
+					if (nombre) {
+						setNombre(nombre);
+					} else {
+						const errMsg =
+							"No se pudo obtener la información del DNI";
+						setError(errMsg);
+						toast.error(errMsg);
+					}
+				} catch (err) {
+					console.error("Error al consultar DNI:", err);
+					const errMsg =
+						"Error al consultar el DNI. Intente nuevamente.";
+					setError(errMsg);
+					toast.error(errMsg);
+				} finally {
+					setIsLoading(false);
+				}
+			};
+			fetchDniData();
+		}
+	}, [tipoDocumento, numeroDocumento, nombre]);
+
+	useEffect(() => {
+		if (tipoDocumento === "ruc" && numeroDocumento.length !== 11) {
+			setNombre("");
+		}
+		if (
+			tipoDocumento === "ruc" &&
+			numeroDocumento.length === 11 &&
+			nombre
+		) {
+			return;
+		}
+		if (tipoDocumento === "ruc" && numeroDocumento.length === 11) {
+			const fetchRucData = async () => {
+				setIsLoading(true);
+				setError(null);
+				try {
+					const nombre = await getRUCData(numeroDocumento);
+					if (nombre) {
+						setNombre(nombre);
+					} else {
+						const errMsg =
+							"No se pudo obtener la información del RUC";
+						setError(errMsg);
+						toast.error(errMsg);
+					}
+				} catch (err) {
+					console.error("Error al consultar RUC:", err);
+					const errMsg =
+						"Error al consultar el RUC. Intente nuevamente.";
+					setError(errMsg);
+					toast.error(errMsg);
+				} finally {
+					setIsLoading(false);
+				}
+			};
+			fetchRucData();
+		}
+	}, [tipoDocumento, numeroDocumento, nombre]);
+	const fetchDenucias = async () => {
+		try {
+			setIsLoading(true);
+			const response = await authApi.get("/search", {
+				params: {
+					numeroDocumento: numeroDocumento,
+				},
+			});
+			if (response.data && Array.isArray(response.data)) {
+				const denunciasFormateadas = response.data.map(
+					(denuncia: Denuncias) => ({
+						id: denuncia.id,
+						tracking_code: denuncia.tracking_code,
+						motivo_id: denuncia.motivo_id,
+						motivo:
+							motivos.find(
+								(motivo) => motivo.id === denuncia.motivo_id
+							)?.nombre || "",
+						descripcion: denuncia.descripcion,
+						fecha_registro: denuncia.fecha_registro,
+						estado: denuncia.estado,
+					})
+				);
+				setDenunciaData(denunciasFormateadas);
+				setError(null);
+			} else {
+				setDenunciaData([]);
+			}
+		} catch (error) {
+			console.error("Error al obtener las denuncias:", error);
+			if (error instanceof Error) {
+				setError(
+					error.message || "Ocurrió un error al obtener las denuncias"
+				);
+			} else {
+				setError("Ocurrió un error al obtener las denuncias");
+			}
+			toast.error("Error al cargar las denuncias", {
+				description:
+					"No se pudieron cargar las denuncias, por favor intenta de nuevo",
+			});
+		} finally {
+			setIsLoading(false);
+		}
+	};
+	const handleSearchClick = useCallback(async () => {
+		if (!tipoDocumento) {
+			toast.error("Por favor, selecciona un tipo de documento.");
+			return;
+		}
+		if (!numeroDocumento) {
+			toast.error("Por favor, ingresa un número de documento válido.");
+			return;
+		}
+		if (
+			(tipoDocumento === "dni" && numeroDocumento.length !== 8) ||
+			(tipoDocumento === "ruc" && numeroDocumento.length !== 11)
+		) {
+			toast.error(
+				`El ${tipoDocumento.toUpperCase()} debe tener ${
+					tipoDocumento === "dni" ? "8" : "11"
+				} dígitos`
+			);
+			return;
+		}
+        await fetchDenucias();
+	}, [tipoDocumento, numeroDocumento, fetchDenucias]);
+	return {
+		tipoDocumento,
+		numeroDocumento,
+		nombre,
+		isLoading,
+		denunciaData,
+		error,
+		handleTipoDocumento,
+		handleDocumentoChange,
+		handleName,
+        handleSearchClick
+	};
+};
--- frontend/src/pages/Admin/SearchDenuncia.tsx
+++ frontend/src/pages/Admin/SearchDenuncia.tsx
@@ -1,35 +1,126 @@
+import { Loader } from "../../Components/Loaders/Loader";
+import { useSearchDenuncia } from "../../hooks/Admin/Denuncias/useSearchDenuncia";
 export const SearchDenuncia = () => {
+	const {
+		numeroDocumento,
+		tipoDocumento,
+		handleDocumentoChange,
+		handleName,
+		nombre,
+		handleTipoDocumento,
+		error,
+		isLoading,
+		denunciaData,
+		handleSearchClick
+	} = useSearchDenuncia();
 	return (
-		<div className="container mx-auto px-4 py-6 max-w-3xl">
+		<div className="container mx-auto my-8 px-4">
 			<h2 className="text-2xl text-center font-bold mb-6 text-gray-800 font-(family-name:--titles) animate__animated animate__fadeInDown">
-				Buscar Denuncia
+				Buscar denuncia
 			</h2>
-			<form
-				className="rounded-lg p-6 relative"
-				// onSubmit={handleSubmit}
-			>
+			<div className="space-y-8 max-w-3xl my-0 mx-auto">
+				<div className="space-y-2">
+					<label className="mb-2" htmlFor="tipo-documento">
+						Tipo de Documento de Identidad del Denunciado
+						<span className="text-red-500 font-black text-xl">
+							*
+						</span>
+					</label>
+					<select
+						name="tipo-documento"
+						id="tipo-documento"
+						value={tipoDocumento}
+						onChange={(e) => handleTipoDocumento(e.target.value)}
+						className="w-full cursor-pointer p-[1em] border-2 border-solid border-(--gray-light) rounded-lg outline-none bg-transparent text-(--secondary-color) focus:ring-2 focus:ring-(--primary-color) focus:border-(--primary-color) transition-all duration-300 ease-in-out"
+					>
+						<option
+							value=""
+							disabled
+							className="text-(--gray-light)"
+						>
+							Seleccione una opción
+						</option>
+						<option value="dni">DNI</option>
+						<option value="ruc">RUC</option>
+						<option value="carnet-extranjeria">
+							Carnet de Extranjería
+						</option>
+					</select>
+				</div>
 				<div className="space-y-2 relative">
 					<input
 						type="text"
-						className="w-full p-3.5 border-2 border-solid border-(--gray-light) rounded-lg outline-none bg-transparent focus:ring-2 focus:ring-(--primary-color) focus:border-(--primary-color) transition-all duration-300 ease-in-out form-part white-bg-focus"
+						className="w-full p-3.5 border-2 border-solid border-(--gray-light) rounded-lg outline-none bg-transparent focus:ring-2 focus:ring-(--primary-color) focus:border-(--primary-color) transition-all duration-300 ease-in-out form-part"
 						placeholder=" "
-						// value={trackingCode}
-						// onChange={handleInputChange}
-						// minLength={20}
-						// maxLength={20}
-						// disabled={trackingLoading}
-						required
+						value={numeroDocumento}
+						onChange={handleDocumentoChange}
+						minLength={tipoDocumento === "dni" ? 8 : 11}
+						maxLength={
+							tipoDocumento === "dni"
+								? 8
+								: tipoDocumento === "ruc"
+								? 11
+								: 20
+						}
 					/>
 					<label className="absolute top-[45%] left-[1em] px-1.5 py-0 pointer-events-none bg-transparent text-(--gray-light) text-base transform -translate-y-1/2 transition-all duration-300 ease-in-out">
-						DNI del Denunciado
+						Nro de Documento de Identidad
 						<span className="text-red-500 font-black">*</span>
 					</label>
-					<button className="absolute right-2 top-[44%] transform -translate-y-1/2 bg-(--secondary-color) text-white font-semibold px-4 py-2 rounded-lg cursor-pointer hover:scale-110 hover:bg-(--primary-color) transition-all ease-in-out duration-300">
-						<i className="fa-solid fa-magnifying-glass"></i>
+					{isLoading && <Loader isBtn={false} />}
+				</div>
+				{error && <p className="text-red-500 text-sm">{error}</p>}
+				{tipoDocumento !== "dni" && tipoDocumento !== "ruc" ? (
+					""
+				) : (
+					<div className="space-y-2 relative">
+						<input
+							type="text"
+							className="w-full p-3.5 border-2 border-solid border-(--gray-light) rounded-lg outline-none bg-transparent focus:ring-2 focus:ring-(--primary-color) focus:border-(--primary-color) transition-all duration-300 ease-in-out form-part"
+							placeholder=" "
+							value={nombre}
+							disabled={isLoading}
+							readOnly={
+								tipoDocumento === "dni" ||
+								tipoDocumento === "ruc" ||
+								tipoDocumento === ""
+							}
+							onChange={
+								tipoDocumento === "dni" ||
+								tipoDocumento === "ruc" ||
+								tipoDocumento === ""
+									? undefined
+									: handleName
+							}
+						/>
+						<label className="absolute top-[45%] left-[1em] px-1.5 py-0 pointer-events-none bg-transparent text-(--gray-light) text-base transform -translate-y-1/2 transition-all duration-300 ease-in-out">
+							Nombre del Denunciado
+							<span className="text-red-500 font-black text-xl">
+								*
+							</span>
+						</label>
+					</div>
+				)}
+				<div className="flex justify-center md:justify-end items-center">
+					<button onClick={handleSearchClick} className="bg-(--primary-color) cursor-pointer text-white px-4 py-2 rounded hover:bg-(--secondary-color) transition duration-300 ease-in-out flex items-center">
+						{isLoading ? (
+							<Loader isBtn={true} />
+						) : (
+							<>
+								<i className="fas fa-search mr-2"></i>
+								Buscar
+							</>
+						)}
 					</button>
 				</div>
-                <div className="filters"></div>
-			</form>
+			</div>
+			<p>
+				{
+					denunciaData.length === 0 && !isLoading
+						? "No se encontraron resultados"
+						: "Si se encontraron aña"
+				}
+			</p>
 		</div>
 	);
 };
