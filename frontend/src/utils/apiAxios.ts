66d3fc0d593531c06323b9aceeb45706eae7aa56
Diego17 <diegoalbertocastropastor@gmail.com>
2025-04-16 08:49:26 -0500

    Fix incorrect string interpolation in apiAxios base URL

--- frontend/src/utils/apiAxios.ts
+++ frontend/src/utils/apiAxios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 const BASE_URL = import.meta.env.VITE_API_BASE_URL
 
 const api = axios.create({
-	baseURL: `"${BASE_URL}/form/"`,
+	baseURL: `${BASE_URL}/form/`,
 	headers: {
 		"Content-Type": "application/json",
 	},
