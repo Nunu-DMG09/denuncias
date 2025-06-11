b78115b3cf1e93e7cecd6cf8b8a1cf4e5c509372
Julian Burga Bracamonte 🤓 <julianburgabracamonte@gmail.com>
2025-06-11 18:14:50 -0500

    Update AdministradoresModel.php

--- backend/app/Models/denuncias/AdministradoresModel.php
+++ backend/app/Models/denuncias/AdministradoresModel.php
@@ -22,7 +22,7 @@ class AdministradoresModel extends Model
         'estado'
     ];
     // Dates
-    protected $useTimestamps = false;
+    protected $useTimestamps = true;
     protected $dateFormat    = 'datetime';
     protected $createdField  = 'created_at';
     protected $updatedField  = 'updated_at';
